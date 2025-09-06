540ea7ed378b65f5f3e60bae6a917226a4906bce
Coutinho-Veenal <coutinhoveenal@gmail.com>
2025-09-06 16:17:56 +0530

    Delete controllers directory

--- controllers/cartController.js
+++
@@ -1,5 +0,0 @@
-const db = require('../config/db'); // This connects to MySQL
-
-exports.viewCart = (req, res) => res.send('Cart page');
-exports.addToCart = (req, res) => res.send('Added to cart');
-exports.checkout = (req, res) => res.send('Checked out');
--- controllers/userController.js
+++
@@ -1,5 +0,0 @@
-const db = require('../config/db'); // This connects to MySQL
-
-exports.dashboard = (req, res) => res.send('User Dashboard');
-exports.showEdit = (req, res) => res.send('Edit Profile page');
-exports.updateProfile = (req, res) => res.send('Profile updated');
